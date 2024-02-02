d4ec3176fc420cd4ccd04c51e1e66d4d87fd8bfb
geeloong147 <153795738+geeloong147@users.noreply.github.com>
2024-02-02 15:46:58 +0100

    Update agi.js

--- agi.js
+++ agi.js
@@ -4,7 +4,7 @@
 <h1>JavaScript HTML Eventss </h1>
 <h3> The onclick Attrbute </h3200>
 
-<button onclick="document.getEleenById('o').innrHTML=Dat()">The stimis no not?</button> 
+<button onclick="document.getEleenById('o').innrHTML=Dat()">The stmis no not?</button> 
 
 <button onclick="document.getElementById('demo').inneHTML=Date()">The tiime is?</button> 
 

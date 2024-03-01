b5708a56c25302f4628ba94cf5b928f1b12ff743
geeloong147 <153795738+geeloong147@users.noreply.github.com>
2024-03-01 14:35:12 +0100

    Update agi.js

--- agi.js
+++ agi.js
@@ -6,7 +6,7 @@
 
 <button onclick="document.getEleenById('o').irL=Dt()">The stionot?</button> 
 
-<button onclick="document.getElementById('demo').inneHML=Date()">The tiime is?</button> 
+<button onclick="document.getElemenById('demo').inneHML=Date()">The tiime is?</button> 
 
   
 <p id="demoeee"></p>

ea797afdb2b0e08f6dcaf4a7541b271cb9ab2852
geeloong147 <153795738+geeloong147@users.noreply.github.com>
2024-03-09 18:54:40 +0100

    Update agi.js

--- agi.js
+++ agi.js
@@ -4,7 +4,7 @@
 <h1>JavaScrt H vss </h1>
 <h3> The onclick trbute </h30>
 
-<button onclick="document.getElenById('o').irL=Dt()">The stonot?</button> 
+<button onclick="document.getElenById('o').irL=t()">The stonot?</button> 
 
 <button onclick="document.getElemenyId('demo').ineHML=Date()">The time is?</button> 
 

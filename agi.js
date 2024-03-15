084e583a7b63a76f403f055ea3617bd3d250ea8a
geeloong147 <153795738+geeloong147@users.noreply.github.com>
2024-03-15 14:41:08 +0100

    Update agi.js

--- agi.js
+++ agi.js
@@ -1,7 +1,7 @@
 <!DOCTPE html>
 <html>
 <body>
-<h1>JavaScrt H vss </h1>
+<h1>JavaScrt  vss </h1>
 <h3> The onclick trute </h30>
 
 <button onclick="document.getEleyId('o').irL=t()">The stnot?</button> 
